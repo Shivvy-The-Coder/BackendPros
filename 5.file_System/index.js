f9850624f7f86552e00e9d02b50c762741c08049
Shivvy-The-Coder <krshivam455@gmail.com>
2025-02-13 13:53:42 +0530

    file system codes has been added

--- 5.file_System/index.js
+++ 5.file_System/index.js
@@ -1,22 +1,27 @@
-
 const fs = require("fs");
 const path = require("path");
 // fs module will be used for reading , writng deleting the fils/ folders
 // path module willl be used for the manipulation of the folder paths andn file path
 
-
 const dirpath = path.dirname(__filename);
 console.log(dirpath);
 
-
-
-const datafolder = path.join(__dirname, "data"); 
-if (!fs.existsSync(datafolder)) { //checking id the foldername data exist inside of the folder or not
-    fs.mkdirSync(datafolder); //if not than create a new folder and join it ith the current directory
-    console.log("data folder has been created");//message that file has been created
-} 
-else console.log(datafolder);//of the folder already existed then simple print its directory path
+const datafolder = path.join(__dirname, "data");
+if (!fs.existsSync(datafolder)) {
+  //checking id the foldername data exist inside of the folder or not
+  fs.mkdirSync(datafolder); //if not than create a new folder and join it ith the current directory
+  console.log("data folder has been created"); //message that file has been created
+} else console.log(datafolder); //of the folder already existed then simple print its directory path
 
 const filepath = path.join(datafolder, "example.txt");
 fs.writeFileSync(filepath, "hello from node js");
 console.log("file create succesfully");
+
+const readcontent = fs.readFileSync(filepath, "utf8");
+console.log(readcontent);
+
+fs.appendFileSync(filepath, "\n this is the next line o the current file ");
+
+console.log("new file content added");
+
+console.log(fs.readFileSync(filepath, "utf8"));
