b6620f9744502686b0ad3859de146f52b21f76a2
Shivam kumar das <krshivam455@gmail.com>
2025-02-19 12:18:23 +0530

    folder has been shfted to another directory

--- 5.file_System/index.js
+++ 5.file_System/index.js
@@ -17,11 +17,21 @@ const filepath = path.join(datafolder, "example.txt");
 fs.writeFileSync(filepath, "hello from node js");
 console.log("file create succesfully");
 
+// to read gthe file inside gthan we can use the readfilesync
 const readcontent = fs.readFileSync(filepath, "utf8");
 console.log(readcontent);
 
+// if we want to wrote something to the file insdie dof the folder than we can use the appendfilesync
 fs.appendFileSync(filepath, "\n this is the next line o the current file ");
 
 console.log("new file content added");
 
 console.log(fs.readFileSync(filepath, "utf8"));
+
+// async way od creating the file
+const asyncfilepath = path.join(datafolder, "async-example.txt");
+fs.writeFile(asyncfilepath, "hello m async fiel method has been used here"),
+  (err) => {
+    if (err) throw err;
+    console.log("error has been detected");
+  };
