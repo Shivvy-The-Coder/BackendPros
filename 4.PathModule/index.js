1426818a95bb14b22930316a858d47d23428cd5b
Shivam kumar das <krshivam455@gmail.com>
2025-02-19 23:48:14 +0530

    path structure has been fulll done here

--- 4.PathModule/index.js
+++ 4.PathModule/index.js
@@ -5,6 +5,7 @@ console.log(__filename)
 
 
 
+const { log } = require("console");
 // get the path module herer
 const path = require("path");
 console.log("directory path ",path.dirname(__filename))
@@ -17,3 +18,19 @@ console.log("file name", path.basename(__filename))
 
 // to get the file extension name
 console.log("file extension ", path.extname(__filename))
+
+
+// how tp join path in node 
+const joinpath = path.join("/user","documents","node","projects");
+console.log("JOINED PATH ", joinpath);
+
+
+// thsis how we resolve yth epath , resolving mean givng the entire path from the drive
+const resolvepath = path.resolve("user", "documents", "node", "projects");
+console.log("resolved path ", resolvepath);
+
+
+// normalizing the path, this will simplif the path and aviud the going and comming in same directiry by moving to
+// a single direcrory onlu once
+const normalizepath = path.normalize("/user/.documents/../node/projects")
+console.log("Normalize path", normalizepath);
