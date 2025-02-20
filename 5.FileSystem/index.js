72ee447f7074f69e0a86dd074f446bbc203e8865
Shivam kumar das <krshivam455@gmail.com>
2025-02-20 22:15:47 +0530

    comments of the file ha been added and also the file path folder ha been updated

---
+++ 5.FileSystem/index.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const path = require("path")
+
+// creating a folder inside of of the current directory
+const datafolder = path.join(__dirname, "data");
+
+// checking if the file exists and if doesnt than fs.mkdirsync will create a new folder name data 
+if(!fs.existsSync(datafolder))
+        {
+            fs.mkdirSync(datafolder);
+            // a message telling taht the fodler has been created
+            console.log("data folder has been created");
+        }
+
+
+// now after the folder has been created we need to create a file , herer we are creating a file with name example.txt
+const filepath = path.join(datafolder, "example.txt");
+// this is synchronous 
+
+// now to wrote inside of the file we need to use writefileSync with the filepath that ha sbeen joined earlier
+fs.writeFileSync(filepath, 'Hello from node js');
+console.log("file has been created succesfully");
+
