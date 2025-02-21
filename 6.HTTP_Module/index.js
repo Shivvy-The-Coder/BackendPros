ca8fe2113b6a981f8ff2ad91bbb13e7c4f8f5965
Shivam kumar das <krshivam455@gmail.com>
2025-02-21 12:29:59 +0530

    http module work has been done herer

--- 6.HTTP_Module/index.js
+++ 6.HTTP_Module/index.js
@@ -5,5 +5,13 @@ const http = require("http")
 //  we will et http req and response object, eq mean the data ha sbeen passed from client to server  and responese mean
 // sending data from server to client
 const server = http.createServer((req,res)=>{
+    console.log(req,'req');
+    res.writeHead(200, {'content-Type':'text-plain'});
+    res.end("hello Node js from http module");
+});
+
+const port = 3000;
+server.listen(port, ()=>{
+    console.log(`server is listening to port ${port}` );
     
 })
