673e123f837b3d0635a548db662baf9e235a8088
Shivam kumar das <krshivam455@gmail.com>
2025-02-21 12:13:30 +0530

    files has been added

--- 6.HTTP_Module/index.js
+++ 6.HTTP_Module/index.js
@@ -1,1 +1,9 @@
+// herer we are importing the http module
 const http = require("http")
+
+// creating a http server
+//  we will et http req and response object, eq mean the data ha sbeen passed from client to server  and responese mean
+// sending data from server to client
+const server = http.createServer((req,res)=>{
+    
+})
