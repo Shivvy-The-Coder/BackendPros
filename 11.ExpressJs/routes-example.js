bdd3b41524a3b400c78e14c5c34664fc30404f32
Shivam kumar das <krshivam455@gmail.com>
2025-02-27 02:43:11 +0530

    express js codes has been added

--- 11.ExpressJs/routes-example.js
+++ 11.ExpressJs/routes-example.js
@@ -23,8 +23,37 @@ app.get('/products',(req,res)=>{
             label:'product3'
         },
     ]
-    res.json();
+    res.json(products);
 });
+
+
+
+// get a single product, its a dynamic api route
+app.get("/products/:id",(req,res)=>{
+    const productId = parseInt(req.params.id);
+    // here id should be same as the product/:id whoch has been mentioned earlier
+
+    const products=[
+        {
+            id:1,
+            label:'product1'
+        },
+        {
+            id:2,
+            label:'product2'
+        },
+        {
+            id:4,
+            label:'product3'
+        },
+    ]
+    const getsingleProduct = products.find(product=>product.id===productId)
+    if(getsingleProduct)
+            res.json(getsingleProduct);
+    else
+            res.status(404).send("product is not found, try another ic")
+}
+)
 
 const port = 3000;
 app.listen(port,()=>{
