cee49f207d25a1c92a115404c38ac553945d300c
Shivam kumar das <krshivam455@gmail.com>
2025-02-27 02:24:03 +0530

    express js codes has been added

--- 11.ExpressJs/routes-example.js
+++ 11.ExpressJs/routes-example.js
@@ -6,6 +6,26 @@ app.get('/',(req,res)=>{
     res.send("Welcome to our homepage");
 });
 
+// get all prodcuts
+app.get('/products',(req,res)=>{
+    // res.send("Welcome to our homepage");
+    const products=[
+        {
+            id:1,
+            label:'product1'
+        },
+        {
+            id:2,
+            label:'product2'
+        },
+        {
+            id:4,
+            label:'product3'
+        },
+    ]
+    res.json();
+});
+
 const port = 3000;
 app.listen(port,()=>{
     console.log(`server  is running at port ${port}`);
