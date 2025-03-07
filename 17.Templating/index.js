64ed5b8c9628315632aac274ddb151ef8a83b706
Shivam <139750193+Shivvy-The-Coder@users.noreply.github.com>
2025-03-07 20:51:35 +0530

    ejs commits has been done

---
+++ 17.Templating/index.js
@@ -0,0 +1,3 @@
+// EJS is a templating language , we use templaing so that the data passed on by client from thier side can be regeclected in our preexisiting pagesor static pages that are present inside our 
+// file, usinh this we can write bits of javascript inside of html file on;y thingtaht differs is trhat the file name ends with .ejs 
+// that is it will be index.ejs
