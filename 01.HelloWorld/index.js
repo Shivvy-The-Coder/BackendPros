9e8a746ac8ac851f9e27851f528b7283b0b8c517
Shivam <139750193+Shivvy-The-Coder@users.noreply.github.com>
2025-04-04 23:58:57 +0530

    Update index.js
    updated

--- 01.HelloWorld/index.js
+++ 01.HelloWorld/index.js
@@ -2,6 +2,7 @@ console.log("Hellow World");
 
 const array = [1, 2, 3, 4];
 console.log(array, "1.array");
+console.log(array, "1.array");
 setInterval(() => {
   console.log("2.This message has been printed after 2 second of earlier display");
 }, 2000);
